c226c1c12f62715127a9a404babbbfdbf2aa5d21
GILZX <gil.war66@gmail.com>
2022-07-06 23:38:46 -0600

    secondComitV2

--- validacion.js
+++ validacion.js
@@ -1,1 +1,23 @@
 //Haz tú validación en javascript acá
+
+const nombre=document.getElementById("txt_nombre")
+const asunto=document.getElementById("txt_asunto")
+const emai=document.getElementById("txt_email")
+const mensaje=document.getElementById("txt_mensaje")
+const boton=document.querySelector(".formcontato__botao")
+
+
+boton.addEventListener('click',function (){
+    if(nombre.textContent!="" && asunto.textContent!="" && emai.textContent!=""){
+        alert("Datos Enviados Correctamente")
+    }else{
+        alert("No Dejar ningun campo sin contenido")
+    }
+    
+
+
+})
+
+
+
+
